5ca75b04b0b70c798f558990536fbec434d20341
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-11-22 10:34:40 -0800

    Updating examples and setting up lib for client side usage

--- examples/parsesvg.js
+++ examples/parsesvg.js
@@ -1,14 +1,15 @@
 var fs = require( 'fs' ),
-    parser = require( '../src/parser' ),
-     util = require( 'util' ),
+    util = require( 'util' ),
     vsvg = require( '../src/' );
 
 fs.readFile( __dirname + '/chart.svg', { encoding: 'utf8' }, function( err, file ) {
     if ( err ) throw err;
-    var parsed = parser.parse( file );
-    // console.log( util.inspect( parsed, { depth: Infinity, colors: true } ) );
 
-    var elem = eachTag( parsed[ 0 ] );
+    var parserElems = vsvg.parse( file ),
+        elem = parserElems[ 0 ];
+    // console.log( util.inspect( parserELems, { depth: Infinity, colors: true } ) );
+
+    styleElem( elem );
     elem.setAttribute( 'vsvg', 'true' );
     
     fs.writeFile( './examples/converted-chart.svg', elem.toHTML(), console.log.bind( console ) );
@@ -17,69 +18,54 @@ fs.readFile( __dirname + '/chart.svg', { encoding: 'utf8' }, function( err, file
 
 var colorArray = [ '#b1b2b3', '#5babdd', '#33aa7b', '#2e76a4' ];
 
-function eachTag( tag ) {
-    
-    var elem;
-    if ( tag.tagName ) {
-        elem = vsvg[ tag.tagName ]( tag.attributes );
-        if ( elem.children ) {        
-            for( var i = 0; i < tag.children.length; i += 1 ) {
-                var _elem = eachTag( tag.children[ i ] );
-                if ( typeof _elem === 'string' ) {
-                    elem.innerText = _elem;
-                } else {
-                    elem.appendChild( _elem );
-                }
-            }
-        }
+function styleElem( elem ) {
 
-        if ( elem.tagName === 'svg' ) {
-            var rect = vsvg.rect( {
-                width: '981',
-                height: '305',
-                fill: 'white'
-            } );
-            elem.insertBefore( rect, elem.children[ 0 ] );
-        }
-
-        if ( elem.tagName === 'polygon' ) {
-            elem.setAttribute( 'fill', '#b1b2b3' );
-        }
-
-        if ( elem.tagName === 'foreignObject' ) {
-            var attributes = elem.attributes;
-            attributes.style = {
-                dx: '100',
-                dy: '100',
-                'font-family': 'sans',
-                padding: '5px'
-            };
-            console.log( attributes );
-            var text = vsvg.text( attributes );
-            text.setAttribute( 'text-anchor', 'start' );
-            text.innerText = elem.innerText;
-            elem = text;
-        }
-
-
-        if ( elem.tagName === 'line' ) {
-            if ( elem.attributes.class === 'ct-bar' ) {
-                elem.setAttribute( 'stroke', colorArray.shift() );
-                elem.setAttribute( 'stroke-width', '5%' );                
-            }
-            else {
-                elem.setAttribute( 'stroke', 'rgba(0,0,0,0.2)' );
-                elem.setAttribute( 'stroke-width', '1' );                
-                elem.setAttribute( 'stroke-dasharray', '2' );
-            }
-        }
-
-
-
-        return elem;
+    if ( !elem.tagName ) {
+        return;
     }
 
-    return tag.text;
+    elem.children.forEach( styleElem );
+
+    if ( elem.tagName === 'svg' ) {
+        var rect = vsvg.rect( {
+            width: '981',
+            height: '305',
+            fill: 'white'
+        } );
+        elem.insertBefore( rect, elem.children[ 0 ] );
+    }
+
+    if ( elem.tagName === 'polygon' ) {
+        elem.setAttribute( 'fill', '#b1b2b3' );
+    }
+
+    if ( elem.tagName === 'foreignObject' ) {
+        var attributes = elem.attributes;
+        attributes.style = {
+            dx: '100',
+            dy: '100',
+            'font-family': 'sans',
+            padding: '5px'
+        };
+        var text = vsvg.text( attributes );
+        text.setAttribute( 'text-anchor', 'start' );
+        text.innerText = elem.innerText;
+        elem = text;
+    }
+
+
+    if ( elem.tagName === 'line' ) {
+        if ( elem.attributes.class === 'ct-bar' ) {
+            elem.setAttribute( 'stroke', colorArray.shift() );
+            elem.setAttribute( 'stroke-width', '5%' );                
+        }
+        else {
+            elem.setAttribute( 'stroke', 'rgba(0,0,0,0.2)' );
+            elem.setAttribute( 'stroke-width', '1' );                
+            elem.setAttribute( 'stroke-dasharray', '2' );
+        }
+    }
+    
 
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
 // export out src svg
+var vsvg = require( './src/' );
 
-module.exports = require( './src/' );
+module.exports = vsvg;
+
+if ( typeof window === 'object' ) {
+    window.vsvg = vsvg;
+}
